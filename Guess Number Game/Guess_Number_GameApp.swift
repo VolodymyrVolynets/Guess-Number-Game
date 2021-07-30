a36c403ae019edecd47e8130d522a111094cb061
Volodymyr Volynets <volodymyr.volynets@student.griffith.ie>
2021-07-30 09:36:52 +0300

    Initial Commit

---
+++ Guess Number Game/Guess_Number_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_Number_GameApp.swift
+//  Guess Number Game
+//
+//  Created by Vova on 30.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Guess_Number_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
